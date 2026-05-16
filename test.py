b2db27ed7c0bde967654be984e2806c3ff9f333a
Ivan Ng <231692836+cybercommits@users.noreply.github.com>
2026-05-16 00:44:23 +0000

    Add type checking and string conversion examples to test script

--- test.py
+++ test.py
@@ -12,4 +12,16 @@ print (2 + 2 * 2 ** 2 / 2) #6.0
 print (2 + 2 * 2 ** 2 / 2 - 2) #4.0
 print (2 + 2 * 2 ** 2 / 2 - 2 + 2) #6.0
 print (2 + 2 * 2 ** 2 / 2 - 2 + 2 * 2) #8.0
+print (type(2 + 2))
+print (type('Python'))
+print (type(2+3.1))
+print (type('1'))
+print (type(int('1')))
+#print (type(int('1.5'))) #ValueError: invalid literal for int() with base 10: '1.5'
+print (type(float('1.5')))
+print (str(1.5))
+print (str(1.5) + ' is a float')
+
+
+
 
