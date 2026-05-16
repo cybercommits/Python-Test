2798344fbf27f754f1cc04daf617c131dd894136
Ivan Ng <231692836+cybercommits@users.noreply.github.com>
2026-05-16 00:53:52 +0000

    Add help function call for int type and examples in test1.py

--- test1.py
+++ test1.py
@@ -1,1 +1,2 @@
-help(int)
+int('1.23') #ValueError: invalid literal for int() with base 10: '1.23'
+#The string '1.23' (which seems to represent a floating point number) cannot be converted directly to an integer.
