3fe9973587f90547dbbc4f05527ad9ff1d2204de
Ivan Ng <231692836+cybercommits@users.noreply.github.com>
2026-05-16 00:26:34 +0000

    Add comments to arithmetic operations in test script

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
-print (2 + 2)
-print (2 * 2)
-print (2 - 2)
-print (2 /2)
-print (2 ** 2)
-print (2 % 2)
-print (2 // 2)
-print (2 + 2 * 2)
-print ((2 + 2) * 2)
-print (2 + 2 * 2 ** 2)
-print (2 + 2 * 2 ** 2 / 2)
-print (2 + 2 * 2 ** 2 / 2 - 2)
-print (2 + 2 * 2 ** 2 / 2 - 2 + 2)
+print (2 + 2) #4
+print (2 * 2) #4
+print (2 - 2) #0
+print (2 /2) #1.0
+print (2 ** 2) #4
+print (2 % 2) #0
+print (2 // 2) #1
+print (2 + 2 * 2) #6
+print ((2 + 2) * 2) #8
+print (2 + 2 * 2 ** 2) #10
+print (2 + 2 * 2 ** 2 / 2) #6.0
+print (2 + 2 * 2 ** 2 / 2 - 2) #4.0
+print (2 + 2 * 2 ** 2 / 2 - 2 + 2) #6.0
+print (2 + 2 * 2 ** 2 / 2 - 2 + 2 * 2) #8.0
+
